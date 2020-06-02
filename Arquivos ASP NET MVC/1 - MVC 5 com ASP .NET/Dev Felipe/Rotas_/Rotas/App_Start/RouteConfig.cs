f87c8d1fd9fbc468eb30180d4edc2a4a1134c9dc
Felipe Dutra <felipe.franz.silva@hotmail.com>
2020-06-01 22:42:02 -0300

    Teste

---
+++ Arquivos ASP NET MVC/1 - MVC 5 com ASP .NET/Dev Felipe/Rotas_/Rotas/App_Start/RouteConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Rotas
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Todas as Noticias", //Nome de definição
+                url: "noticias/", //Url no navegador
+                defaults: new { controller = "Home", action = "TodasasNoticias" } //Nome do Controller e nome Action chamada
+            );
+
+            routes.MapRoute(
+                name: "Categoria Especifica", //Nome de definição
+                url: "noticias/{categoria}", //Url no navegador
+                defaults: new { controller = "Home", action = "MostraCategoria" } //Nome do Controller e nome Action chamada
+            );
+
+            routes.MapRoute(
+                name: "Mostra Noticia", //Nome de definição
+                url: "noticias/{categoria}/{titulo}-{noticiaId}", //Url no navegador
+                defaults: new { controller = "Home", action = "MostraNoticia" } //Nome do Controller e nome Action chamada
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
